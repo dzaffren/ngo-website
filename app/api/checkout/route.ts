6677db18679e6a0832b7852c7d1bfad5006c545a
Dzafran <dzafran.bustaman@bnm.gov.my>
2025-12-31 10:14:52 +0800

    update api version stripe

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe'
 
 // Initialize Stripe
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: '2024-12-18.acacia', // Use the latest API version or the one suggested by your terminal
+  apiVersion: '2025-12-15.clover', // Use the latest API version or the one suggested by your terminal
 })
 
 export async function POST(req: Request) {
